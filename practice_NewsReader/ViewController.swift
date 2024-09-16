a6275626f0809c2c6ee2b28d91830048fefce3c7
sgmkhr <mksghr10@gmail.com>
2024-09-16 20:27:37 +0900

    Initial Commit

---
+++ practice_NewsReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice_NewsReader
+//
+//  Created by 菅原実希 on 2024/09/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
