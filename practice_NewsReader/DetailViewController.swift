4a003963e9814005570c7e49169fbab4ac72450d
sgmkhr <mksghr10@gmail.com>
2024-09-16 22:18:44 +0900

    詳細画面

---
+++ practice_NewsReader/DetailViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailViewController.swift
+//  practice_NewsReader
+//
+//  Created by 菅原実希 on 2024/09/16.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    var link:String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let url = URL(string: self.link) {
+            let request = URLRequest(url: url)
+            self.webView.load(request)
+        }
+    }
+}
